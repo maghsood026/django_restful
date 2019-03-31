25a330c63e72cba3c072b13e466b8ebaf7ff4e4e
maghsood026 <maghsood026@yahoo.com>
2019-03-31 21:25:15 +0430

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
 
-
-urlpatterns = [
-    path('get/', views.ArticleViewToGet.as_view(), name="show article"),
-    path('get/<pk>', views.ArticleDetailView.as_view(), name="show detail of article"),
-    path('post/', views.ArticleViewToPost.as_view(), name="post new article"),
-    path('update/<pk>', views.ArticleViewToUpdate.as_view(), name="update article"),
-    path('delete/<pk>', views.ArticleViewToDelete.as_view(), name="delete article")
-]
+router = DefaultRouter()
+router.register('api', views.ArticleViewSet, base_name="articles")
+urlpatterns = router.urls
